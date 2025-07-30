6ddbf17bf018c59c41bd29b491fd1fc8ebf8cd1b
achoi <achoi@indeed.com>
2025-07-30 15:06:04 -0700

    edited homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,26 @@
 import { type JSX } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {Flex, Button, Heading} from "@chakra-ui/react"
 
 const Home = (): JSX.Element => {
   return (
     <>
-      <h1>Fridgy Home</h1>
-      <main>
-        <NavLink to="/addgrocerylist">
-          <button>Create list</button>
-        </NavLink>
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        minH="100vh"
+        p={8}
+        textAlign="center"
+      >
+        <Heading size="2xl">Fridgy Home</Heading>
         <br></br>
-
-        <button>Create fridge</button>
-        <h2>My Grocery Lists</h2>
-        <div>
-          {/* TODO: Map through grocery lists and display them */}
-          <Link to={"/grocerylists/:id"}>
-            <p>List 1</p>
-          </Link>
-        </div>
-
-        {/* TODO: Map through refrigerators and display them */}
-        <h2>My Refrigerators</h2>
-        <Link to={"/refrigerators"}>
-          <p>Ref 1</p>
+        <Link to="/addgrocerylist">
+          <Button>My Grocery Lists</Button>
         </Link>
-      </main>
+        <br></br>
+        <Button>My Refrgerators</Button>
+      </Flex>
     </>
   );
 };
